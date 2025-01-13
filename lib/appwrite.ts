64e40c483990f729a7da06cc7dccbe9ed160b610
Veronika Fedorova <fedorovavv03@gmail.com>
2025-01-13 15:55:56 +0200

    Setup database

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { Account, Avatars, Client, OAuthProvider } from 'react-native-appwrite';
+import { Account, Avatars, Client, Databases, OAuthProvider } from 'react-native-appwrite';
 import * as WebBrowser from 'expo-web-browser';
 import * as Linking from 'expo-linking';
 
@@ -6,6 +6,11 @@ export const config = {
   platform: 'com.vf.restate',
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTIONS_ID,
+  reviesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTIONS_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTIONS_ID,
+  propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTIONS_ID,
 };
 
 export const client = new Client();
@@ -17,6 +22,7 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
+export const databases = new Databases(client);
 
 export async function login() {
   try {
@@ -34,17 +40,19 @@ export async function login() {
       redirectUri
     );
 
-    if(browserResult.type !== 'success') throw new Error('Failed to receive a response. Please, check your information.');
+    if (browserResult.type !== 'success')
+      throw new Error(
+        'Failed to receive a response. Please, check your information.'
+      );
 
     const url = new URL(browserResult.url);
     const secret = url.searchParams.get('secret')?.toString();
     const userId = url.searchParams.get('userId')?.toString();
 
-    if(!secret || !userId) throw new Error('Failed to login');
+    if (!secret || !userId) throw new Error('Failed to login');
 
     const session = await account.createSession(userId, secret);
-    if(!session) throw new Error('Failed to create an account');
-
+    if (!session) throw new Error('Failed to create an account');
   } catch (error) {
     console.error(error);
     return false;
@@ -52,28 +60,28 @@ export async function login() {
 }
 
 export async function logout() {
-    try {
-        await account.deleteSession('current');
-        return true;
-    } catch (error) {
-        console.error(error);
-        return false;
-    }
+  try {
+    await account.deleteSession('current');
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
 
 export async function getCurrentUser() {
-    try {
-        const response = await account.get();
-        if(response.$id) {
-            const userAvatar = avatar.getInitials(response.name);
+  try {
+    const response = await account.get();
+    if (response.$id) {
+      const userAvatar = avatar.getInitials(response.name);
 
-            return {
-                ...response,
-                avatar: userAvatar.toString()
-            }
-        }
-    } catch (error) {
-        console.error(error);
-        return null;
+      return {
+        ...response,
+        avatar: userAvatar.toString(),
+      };
     }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
